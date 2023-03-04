52cfa0b9796174e2af46246c23dbe4f09deb7025
Abhishek razy <abhishekrzy@gmail.com>
2023-03-04 10:05:10 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function convertIndexToWord(indexString) {
-  const alphabet = 'abcdefghijklmnopqrstuvwxyz';
-  const indexArray = indexString.split(',');
-  let wordArray = [];
-  indexArray.forEach(index => {
-    const i = parseInt(index.trim()) - 1;
-    const word = alphabet.charAt(i);
-    wordArray.push(word);
-  });
