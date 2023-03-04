30b8fef0ba12025a7b525b8a091f59bb6495e498
Abhishekrazy <abhishekrzy@gmail.com>
2023-03-04 15:45:30 +0530

    Seperate Js

---
+++ script.js
@@ -0,0 +1,146 @@
+function convertIndexToWord() {
+	const indexString = document.getElementById("index-input").value;
+	const alphabet = "abcdefghijklmnopqrstuvwxyz";
+	const indexArray = indexString.split(" ");
+	let wordArray = [];
+	let finalWord = "";
+
+	indexArray.forEach((index) => {
+		const i = parseInt(index.trim()) - 1;
+		if (i <= 26) {
+			const word = alphabet.charAt(i);
+			wordArray.push(word);
+			finalWord = wordArray.join("");
+		} else {
+			finalWord = "Not a valid index";
+		}
+	});
+	if (finalWord != "Not a valid index") {
+		wordArray = capitalizeFirstLetter(finalWord);
+		lookforthisword(finalWord);
+	}
+	document.getElementById("result").innerHTML = finalWord;
+}
+function letterToIndex() {
+	const input = document.getElementById("index-input").value;
+	lookforthisword(input);
+	console.log("input: ", input);
+	// Split the input string by space into an array of words
+	const words = input.split(" ");
+	console.log("words: ", words);
+	// Array to store the index of each word
+	const indices = [];
+	// Loop through each word, convert it to index, and push to indices array
+	words.forEach((word) => {
+		// Convert the word to lowercase
+		const lowercaseWord = word.toLowerCase();
+		// Get the ASCII code of the first character of the word
+		const asciiCode = lowercaseWord.charCodeAt(0);
+		// Subtract the ASCII code of 'a' to get the index (0-25)
+		const index = asciiCode - 96;
+		indices.push(index);
+	});
+
+	// Join the indices array into a string separated by space
+	const result = indices.join(" ");
+
+	// Display the result in the result element
+	document.getElementById("result").innerHTML = result;
+}
+function capitalizeFirstLetter(str) {
+	return str.toString().charAt(0).toUpperCase() + str.toString().slice(1);
+}
+function lookforthisword(str) {
+	console.log("str: ", str);
+	const apiUrl = "https://api.dictionaryapi.dev/api/v2/entries/en/" + str; //encodeURIComponent();
+
+	// Make a GET request to the API
+	fetch(apiUrl)
+		.then((response) => {
+			if (response.status === 404) {
+				return response.json().then((error) => {
+					console.log(error.title);
+					console.log(error.message);
+					const responseElement = document.getElementById("response");
+					responseElement.innerHTML = `
+							<p style="text-align:center;">${error.title}</p>
+							<p style="text-align:center;">${error.message}</p>
+							<p style="text-align:center;">${error.resolution}</p>
+							`;
+				});
+			}
+			return response.json();
+		})
+		.then((data) => {
+			// Format the response as a string
+			const formattedResponse = JSON.stringify(data, null, 2);
+			const myDiv = document.getElementById("my-audio");
+			myDiv.innerHTML = "";
+			for (let key of data) {
+				// select the div where you want to append the audio element
+
+				for (let phoetics of key.phonetics) {
+					if (phoetics.audio) {
+						console.log(JSON.stringify(phoetics.license.name));
+						const div = document.createElement("div");
+						div.setAttribute(
+							"style",
+							"flex-direction: column; display: flex; flex-wrap: wrap; justify-content: center; align-items: center;"
+						);
+						// create the audio element
+						const audio = document.createElement("audio");
+						audio.setAttribute("style", "padding: 10px;");
+						audio.controls = true;
+						// create the source element and set its attributes
+						const source = document.createElement("source");
+						source.src = phoetics.audio;
+						source.type = "audio/mpeg";
+
+						if (phoetics.license.name) {
+							audio.setAttribute("title", phoetics.license.name);
+						}
+						// append the source element to the audio element
+						audio.appendChild(source);
+
+						const a = document.createElement("a");
+						a.href = phoetics.sourceUrl;
+						a.setAttribute("style", "font: caption; text-decoration: none;");
+						a.setAttribute("id", "url");
+						a.textContent = "Source Url";
+
+						div.appendChild(audio);
+						div.appendChild(a);
+						// append the audio element to the div
+						myDiv.appendChild(div);
+					}
+				}
+			}
+			// Display the response in a <pre> element
+			const responseElement = document.getElementById("response");
+			responseElement.innerHTML = `<p style="text-align:center;">${formattedResponse}</p>`;
+		})
+		.catch((error) => {
+			console.error("Error:", error);
+			console.error("Error:", JSON.stringify(error));
+		});
+}
+// Set the default function to letterToIndex()
+function toggleFunction() {
+	// Get the toggle switch and the button elements
+	const toggle = document.getElementById("checkbox");
+	const heading = document.getElementById("heading");
+
+	if (toggle.checked) {
+		heading.innerText = "Convert Index to Letter";
+	} else {
+		heading.innerText = "Convert Letter to Index";
+	}
+}
+function checkToggle() {
+	const toggle = document.getElementById("checkbox");
+	if (toggle.checked) {
+		convertIndexToWord();
+	} else {
+		letterToIndex();
+	}
+}
