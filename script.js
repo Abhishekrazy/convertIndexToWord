6cf2dd1ec23fefbd772565b1625e79cf25ee0a8e
Abhishek razy <abhishekrzy@gmail.com>
2023-03-04 09:53:36 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+function convertIndexToWord(indexString) {
+
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+
+  const indexArray = indexString.split(',');
+
+  let wordArray = [];
+
+  indexArray.forEach(index => {
+
+    const i = parseInt(index.trim()) - 1;
+
+    const word = alphabet.charAt(i);
+
+    wordArray.push(word);
+
+  });
+
